bee9b5f72f5724347298942261f1e2097b6d06db
Bri8102 <damioniru16@gmail.com>
2020-12-08 00:21:06 -0500

    commit tree 15bf4e8164433fbbad0095d5908fba178338aa19

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+// function submitData(user_name,user_email) {
+    
+//     let configObj = { method: "POST", 
+//                       headers: {
+//                                   "Content-Type" : "application/json",
+//                                   "Accept": "application/json" },
+//                       body : JSON.stringify({name: user_name,email: user_email})
+//                     };
+
+//     return fetch('http://localhost:3000/users',configObj)
+//           .then(function(response) {return response.json();
+// })
+//           .then(function(object) {console.log(object);})
+//           .catch(function(error) {alert('There is an error! Please retry!'); console.log(error.message); });
+// }
+
+
+function submitData(name,email) {
+    let data = { name: name, email:email };
+    let configObj = { method: "POST", 
+                      headers: { "Content-Type": "application/json",
+                                 "Accept": "application/json" },
+                      body: JSON.stringify(data)
+  };
+  return fetch('http://localhost:3000/users',configObj)
+    .then(function(response) {return response.json()})
+    .then(object => document.body.innerHTML += object.id)
+    .catch(error => document.body.innerHTML += error.message);
+}
